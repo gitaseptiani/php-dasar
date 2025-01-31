7aa17c43a99fe745a09e8a8fef0ed1ece273f056
gitaseptiani <megitaaa22@gmail.com>
2025-01-31 19:15:23 +0700

    Tugas 12 Gita Septiani

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,6 +1,12 @@
 <?php
 
 session_start();
+if (!isset($_SESSION['login'])) {
+    if ($_SESSION['login'] != true) {
+        header("Location: login.php");
+        exit;
+    }
+}
 
 $mysqli = new mysqli('localhost', 'root', '', 'tedc');
 
@@ -24,6 +30,7 @@ $result = $mysqli->query("SELECT students.nim, students.nama, study_programs.nam
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Daftar Mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
 <body>
     <div class="container mt-4">
--- tambah_mahasiswa.php
+++ tambah_mahasiswa.php
@@ -1,7 +1,12 @@
 <?php
-// tambah_mahasiswa.php
 
-session_start(); // Mulai session
+session_start();
+if (!isset($_SESSION['login'])) {
+    if ($_SESSION['login'] != true) {
+        header("Location: login.php");
+        exit;
+    }
+}
 
 $mysqli = new mysqli('localhost', 'root', '', 'tedc');
 
@@ -35,6 +40,7 @@ $study_programs = $mysqli->query("SELECT * FROM study_programs");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tambah Mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
 <body>
     <div class="container mt-4">
